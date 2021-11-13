6bddb3d93ca6955215a60033219a92e4754392ed
Nihal-a <nihalavulan1@gamil.com>
2021-11-13 19:30:53 +0530

    Folder structures creted , Icons imported and Sidenav created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-
+import {QueryClientProvider , QueryClient} from "react-query"
+import {BrowserRouter as Router , Route , Switch} from "react-router-dom"
+import Dashboard from "./components/Dashboard/Dashboard";
 function App() {
+
+  const queryClient = new QueryClient()
   return (
-    <h1 >Hello</h1>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Dashboard/>
+          </Route>
+          <Route exact path="/login">
+            <Dashboard/>
+          </Route>
+          <Route exact path="/register">
+            <Dashboard/>
+          </Route>
+        </Switch>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideNav from '../UI/Sidenav/SideNav'
+
+function Dashboard() {
+    return (
+        <div>
+            <SideNav />
+        </div>
+    )
+}
+
+export default Dashboard
