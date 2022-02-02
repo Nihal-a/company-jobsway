30343d6c140a41b3031a0fb98871fa0abd1e05e2
Nihal-a <nihalavulan1@gamil.com>
2022-02-02 10:38:27 +0530

    Api changed

--- src/api/company.js
+++ src/api/company.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL:'http://localhost:4000/api/v1/company/'})
+// const API = axios.create({ baseURL:'http://localhost:4000/api/v1/company/'})
 // const API = axios.create({ baseURL:'https://jobsway-company.herokuapp.com/api/v1/company/'})
+const API = axios.create({ baseURL:'http://companyapi.jobsway.online/api/v1/company'})
+
 
 
 //company
